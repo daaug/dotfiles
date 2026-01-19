fc7a935cfca68380112ae692135a72278a854f9e
Daniel Augusto <daniel.aug.souza@gmail.com>
2026-01-19 10:50:47 -0300

    Upd|New xmonad commands

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,26 +2,35 @@ import XMonad
 import qualified Data.Map as M
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Util.SpawnOnce
-import XMonad.Util.EZConfig (additionalKeysP, remapKeysP)
+import XMonad.Util.EZConfig (additionalKeysP, remapKeysP, removeKeysP)
 -- bar
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
+-- floating window
+import qualified XMonad.StackSet as W
 
 main :: IO ()
 main = do 
-    xmonad $ ewmh $ docks def
+    xmonad $ ewmhFullscreen $ ewmh $ docks def
         { modMask       = mod4Mask
         , keys          = myKeys
 		, layoutHook	= avoidStruts $ layoutHook def -- avoidStruts == tells layouts to leave space for them
         , startupHook   = myStartupHook <+> startupHook def
         }
-        `additionalKeysP`
-        [ ("M-p", spawn "dmenu_run -b -l 10 -nb black -nf white -sb red -sf white")
-        , ("M-<Return>", spawn "urxvt")
+        `removeKeysP` 
+        [ ("M-<Space>")
         ]
         `remapKeysP`
         [ ("M-S-e", "M-S-q")
         , ("M-S-q", "M-S-c")
+        ]
+        `additionalKeysP`
+        [ ("M-p", spawn "dmenu_run -b -l 20 -nb black -nf white -sb red -sf white")
+        , ("M-<Return>", spawn "urxvt")
+        , ("M-x", spawn "xfce4-screenshooter")
+        , ("M-S-<Space>", withFocused toggleFloat)
+        , ("M-m", sendMessage $ JumpToLayout "Full")
+        , ("M-S-m", sendMessage NextLayout)
         ]
 
 myStartupHook :: X ()
@@ -36,4 +45,12 @@ myKeys conf = M.union (keys def conf) (M.fromList
     , ((0, xF86XK_MonBrightnessUp),   spawn "brightnessctl set +10%")
     , ((0, xF86XK_MonBrightnessDown), spawn "brightnessctl set 10%-")
     ])
+
+toggleFloat :: Window -> X ()
+toggleFloat w =
+    windows (\s ->
+        if M.member w (W.floating s)
+            then W.sink w s
+            else W.float w (W.RationalRect 0.1 0.1 0.8 0.8) s
+    )
 
